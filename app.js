c0d4a162f9b3291575ac509a88f7bebb28c47fa0
SUbRAt-2004 <163215796+SUbRAt-2004@users.noreply.github.com>
2025-08-30 23:28:57 +0530

    Added maps and did final security checks

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if(process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+const sanitizeV5 = require('./utils/mongoSanitizeV5.js');
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
@@ -9,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
 
 const userRoutes = require('./routes/users');
@@ -25,20 +30,24 @@ db.once("open", () => {
 
 const app = express();
 
+app.set('query parser', 'extended');
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(sanitizeV5({ replaceWith: '_' }));
 
 const sessionConfig = {
+    name: 'session',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
+        //secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
@@ -46,6 +55,59 @@ const sessionConfig = {
 
 app.use(session(sessionConfig))
 app.use(flash());
+//app.use(helmet({contentSecurityPolicy : false})); //adds in all eleven types of helmet at once
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.tiles.mapbox.com/",
+    // "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    // "https://api.mapbox.com/",
+    // "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net",
+    "https://cdn.maptiler.com/", // add this
+];
+const connectSrcUrls = [
+    // "https://api.mapbox.com/",
+    // "https://a.tiles.mapbox.com/",
+    // "https://b.tiles.mapbox.com/",
+    // "https://events.mapbox.com/",
+    "https://api.maptiler.com/", // add this
+];
+
+app.use(
+    helmet({
+        contentSecurityPolicy: {
+            directives: {
+                defaultSrc: [],
+                connectSrc: ["'self'", ...connectSrcUrls],
+                scriptSrc: ["'self'", "'unsafe-inline'", ...scriptSrcUrls],
+                styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+                workerSrc: ["'self'", "blob:"],
+                objectSrc: [],
+                 imgSrc: [
+    "'self'",
+    "blob:",
+    "data:",
+    "https://res.cloudinary.com/", // if you’re using Cloudinary
+    "https://images.unsplash.com/", // if you’re seeding with Unsplash
+    "https://cdn.maptiler.com/",
+    "https://api.maptiler.com/"
+],
+                fontSrc: ["'self'", "https://cdn.jsdelivr.net"]
+            }
+        }
+    })
+);
+
 
 app.use(passport.initialize());
 app.use(passport.session());
--- seeds/index.js
+++ seeds/index.js
@@ -13,17 +13,45 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0; i<50; i++) {
+    for(let i=0; i<300; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             title: `${i} Camp`,
-            author: '68a5dabcf692c252ff96c9bf',
+            author: '68af5b209ab0d7bf18648893',
             price: price,
+            geometry: {
+                type: "Point",
+                coordinates: [
+                    cities[random1000].longitude,
+                    cities[random1000].latitude,
+                ]
+            },
             description: 'This is a description',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: `https://picsum.photos/400?random=${Math.random()}`,
+            images: [
+      {
+        url: 'https://res.cloudinary.com/djoks5zjd/image/upload/v1756570680/YelpCamp/qjz77ytvixrxoo2laz0z.jpg',
+        filename: 'YelpCamp/qjz77ytvixrxoo2laz0z',
+      
+      },
+      {
+        url: 'https://res.cloudinary.com/djoks5zjd/image/upload/v1756570680/YelpCamp/o65ptjgkg8bctbc0q9mg.jpg',
+        filename: 'YelpCamp/o65ptjgkg8bctbc0q9mg',
+
+      },
+      {
+        url: 'https://res.cloudinary.com/djoks5zjd/image/upload/v1756570680/YelpCamp/hqp5xxoyr9ilfyni7mnh.jpg',
+        filename: 'YelpCamp/hqp5xxoyr9ilfyni7mnh',
+        
+      },
+      {
+        url: 'https://res.cloudinary.com/djoks5zjd/image/upload/v1756570681/YelpCamp/kc6a7ln7vq34fv9qsm1p.jpg',
+        filename: 'YelpCamp/kc6a7ln7vq34fv9qsm1p',
+        
+      }
+    ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'
         })
         await camp.save();
